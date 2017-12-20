3a03dad157ab47244a1682218cc68ef0309ac7fa
jmtimper <34565851+jmtimper@users.noreply.github.com>
2017-12-20 16:22:31 -0500

    Passed Unique Chars in a String question
    Uploaded another solution to a firecode.io test

--- Interview Practice Problems/firecode.io questions/Unique Chars in a String.java
+++ Interview Practice Problems/firecode.io questions/Unique Chars in a String.java
@@ -1,13 +1,19 @@
 // java.util.* has been imported for this problem.
 // You don't need any other imports.
 
-public static boolean areAllCharactersUnique(String str){
-    if(str == null) return true;
-    ArrayList characters = new ArrayList();
 
+/**
+ * Write a method that takes in an input String and returns true if all the characters in the String are unique 
+ * and false if there is even a single repeated character.
+ * The method should return true if the input is null or empty String.
+ */
+public static boolean areAllCharactersUnique(String str){
+    if(str == null || str == "") return true;
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
     for(int i = 0; i < str.length(); i++){
-        if(!characters.contains(str.charAt(i))) characters.add(str.charAt(i));
-        else return false;
+        if(map.containsKey(str.charAt(i))) return false;
+        else map.put(str.charAt(i), 1);
     }
     return true;
+
 }
